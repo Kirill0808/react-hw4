32a9a22fddf35ba00a5db5a0595bfb42af326f4e
kirill0808 <dronov0808@gmail.com>
2022-01-15 19:39:20 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {ContactList} from "./components/ContactList/ContactList";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactList/>
+    </div>
+  );
+}
+
+export default App;
